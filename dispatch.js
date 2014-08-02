e007bb690f459a5407079ca99bc92c5f47cdaf4d
Ian <typppo@gmail.com>
2014-08-02 16:55:30 -0400

    add freebase

--- dispatch.js
+++ dispatch.js
@@ -1,5 +1,6 @@
-var google = require('google');
-var Q = require('q');
+var google = require('google'),
+    Q = require('q'),
+    freebase = require('freebase');
 
 (function setup() {
   google.resultsPerPage = 1;
@@ -11,7 +12,11 @@ exports.process = function(phrase) {
 
   // RIght now just send it to goog.  In the future we might be smarter.
   // TODO Combine deferreds with other search stuff.
-  return search_google(phrase);
+  /*
+  return Q.all(Array.map([search_google, search_freebase, search_freebase_image], function(fn) {
+    fn(phrase);
+  })).
+    */
 }
 
 function search_google(term) {
@@ -36,4 +41,24 @@ function search_drive(term) {
 
 function search_gmail(term) {
 
+}
+
+function search_freebase(term) {
+  var deferred = Q.defer();
+  freebase.description(term, {}, function(desc) {
+    deferred.resolve({
+      desc: desc
+    });
+  });
+  return deferred;
+}
+
+function search_freebase_image(term) {
+  var deferred = Q.defer();
+  freebase.image(term, {}, function(url) {
+    deferred.resolve({
+      url: url
+    });
+  });
+  return deferred;
 }
