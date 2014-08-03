e273045d482f9446c6149271f583c9e32245cfe3
Andy Kearney <akearney@hmc.edu>
2014-08-02 18:10:07 -0700

    fix speech and clean up

--- public/assets/js/speech.js
+++ public/assets/js/speech.js
@@ -27,26 +27,21 @@ $(function() {
 
     recognition.onstart = function() {
       recognizing = true;
-      showInfo('info_speak_now');
       start_img.src = '/assets/images/mic-animate.gif';
     };
 
     recognition.onerror = function(event) {
       if (event.error == 'no-speech') {
         start_img.src = '/assets/images/mic.gif';
-        showInfo('info_no_speech');
         ignore_onend = true;
       }
       if (event.error == 'audio-capture') {
         start_img.src = '/assets/images/mic.gif';
-        showInfo('info_no_microphone');
         ignore_onend = true;
       }
       if (event.error == 'not-allowed') {
         if (event.timeStamp - start_timestamp < 100) {
-          showInfo('info_blocked');
         } else {
-          showInfo('info_denied');
         }
         ignore_onend = true;
       }
@@ -59,19 +54,13 @@ $(function() {
       }
       start_img.src = '/assets/images/mic.gif';
       if (!final_transcript) {
-        showInfo('info_start');
         return;
       }
-      showInfo('');
       if (window.getSelection) {
         window.getSelection().removeAllRanges();
         var range = document.createRange();
         range.selectNode(document.getElementById('final_span'));
         window.getSelection().addRange(range);
-      }
-      if (create_email) {
-        create_email = false;
-        createEmail();
       }
     };
 
@@ -97,7 +86,6 @@ $(function() {
 
   function upgrade() {
     start_button.style.visibility = 'hidden';
-    showInfo('info_upgrade');
   }
 
   var two_line = /\n\n/g;
@@ -111,67 +99,20 @@ $(function() {
     return s.replace(first_char, function(m) { return m.toUpperCase(); });
   }
 
-  function createEmail() {
-    var n = final_transcript.indexOf('\n');
-    if (n < 0 || n >= 80) {
-      n = 40 + final_transcript.substring(40).indexOf(' ');
-    }
-    var subject = encodeURI(final_transcript.substring(0, n));
-    var body = encodeURI(final_transcript.substring(n + 1));
-    window.location.href = 'mailto:?subject=' + subject + '&body=' + body;
-  }
-
-  function copyButton() {
-    if (recognizing) {
-      recognizing = false;
-      recognition.stop();
-    }
-    copy_button.style.display = 'none';
-    copy_info.style.display = 'inline-block';
-    showInfo('');
-  }
-
-  function emailButton() {
-    if (recognizing) {
-      create_email = true;
-      recognizing = false;
-      recognition.stop();
-    } else {
-      createEmail();
-    }
-    email_button.style.display = 'none';
-    email_info.style.display = 'inline-block';
-    showInfo('');
-  }
-
   function startButton(event) {
     if (recognizing) {
       recognition.stop();
       return;
     }
     final_transcript = '';
-    recognition.lang = select_dialect.value;
+    recognition.lang = 'en-US';
     recognition.start();
     ignore_onend = false;
     final_span.innerHTML = '';
     interim_span.innerHTML = '';
     start_img.src = '/assets/images/mic-slash.gif';
-    showInfo('info_allow');
     showButtons('none');
     start_timestamp = event.timeStamp;
-  }
-
-  function showInfo(s) {
-    if (s) {
-      for (var child = info.firstChild; child; child = child.nextSibling) {
-        if (child.style) {
-          child.style.display = child.id == s ? 'inline' : 'none';
-        }
-      }
-      info.style.visibility = 'visible';
-    } else {
-      info.style.visibility = 'hidden';
-    }
   }
 
   var current_style;
@@ -281,4 +222,6 @@ $(function() {
   };
 
   addResultsTemplates(debugResponse);
+
+  $('#start_button').on('click', startButton);
 });
