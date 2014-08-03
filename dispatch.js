f83f81d3c03c38de290cf56e9c8dbe6d543e8f80
Ian <typppo@gmail.com>
2014-08-03 00:48:34 -0400

    fix ud

--- dispatch.js
+++ dispatch.js
@@ -70,6 +70,7 @@ function search_google(term) {
 function search_urban_dictionary(term) {
   var deferred = Q.defer();
   urban(term).first(function(json) {
+    json = json || {};
     deferred.resolve(_.extend(json, {type: 'urban'}));
   });
   return deferred.promise;
